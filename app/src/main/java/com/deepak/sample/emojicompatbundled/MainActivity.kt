16ff775a4ad607a4eccea6893140e52390a3fc4e
Deepak Goyal <er.deepakgoyal786@gmail.com>
2017-11-05 04:22:53 +0530

    final commit

--- app/src/main/java/com/deepak/sample/emojicompatbundled/MainActivity.kt
+++ app/src/main/java/com/deepak/sample/emojicompatbundled/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.deepak.sample.emojicompatbundled
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.text.emoji.EmojiCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.apache.commons.lang3.StringEscapeUtils
+import org.apache.commons.lang3.StringUtils
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val emojiContent: String = "neutral face \uD83D\uDE10"
+    private val emojiContent: String = "Hey \uD83D\uDc83\ud83c\udffb"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val processed = EmojiCompat.get().process(emojiContent)
-        textView.text = processed
+        textView2.text = StringEscapeUtils.unescapeJava(emojiContent)
+
+        EmojiCompat.get().registerInitCallback(object : EmojiCompat.InitCallback() {
+            override fun onInitialized() {
+                super.onInitialized()
+                println("EmojiCompat initialized successfully")
+                val processed = EmojiCompat.get().process(emojiContent)
+                textView4.text = processed
+            }
+
+            override fun onFailed(throwable: Throwable?) {
+                super.onFailed(throwable)
+                Toast.makeText(this@MainActivity, throwable?.message ?: "", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
